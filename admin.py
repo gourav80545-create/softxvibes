e4fb62940591ba5ca88eb2f6f5b8336f05f34b72
gourav80545-create <gourav80545@gmail.com>
2026-06-06 20:34:29 +0530

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,26 @@
+from pyrogram import Client, filters
+from config import Config
+from database import db
+
+def is_owner(func):
+    async def wrapper(client, message):
+        if message.from_user.id != Config.OWNER_ID:
+            await message.reply_text("❌ Only owner!")
+            return
+        return await func(client, message)
+    return wrapper
+
+@Client.on_message(filters.command("addadmin"))
+@is_owner
+async def add_admin(client, message):
+    user_id = message.reply_to_message.from_user.id
+    await db.admins.insert_one({"_id": user_id})
+    await message.reply_text(f"✅ Admin added!")
+
+@Client.on_message(filters.command("adminlist"))
+async def admin_list(client, message):
+    admins = await db.admins.find().to_list(None)
+    text = "👮 ADMINS:\n\n"
+    for admin in admins:
+        text += f"`{admin['_id']}`\n"
+    await message.reply_text(text)
