51f7bc76b6b9e63a625a1e7e36276ee278b719ff
gourav80545-create <gourav80545@gmail.com>
2026-06-07 10:45:52 +0530

    Update callbacks.py

--- callbacks.py
+++ callbacks.py
@@ -1,13 +1,14 @@
-from pyrogram import Client, filters
+from pyrogram import filters
+from client import app
 
-@Client.on_callback_query(filters.regex("music_play"))
+@app.on_callback_query(filters.regex("music_play"))
 async def music_play(client, callback_query):
     await callback_query.answer("▶️ Playing...")
 
-@Client.on_callback_query(filters.regex("music_pause"))
+@app.on_callback_query(filters.regex("music_pause"))
 async def music_pause(client, callback_query):
     await callback_query.answer("⏸ Paused...")
 
-@Client.on_callback_query(filters.regex("close"))
+@app.on_callback_query(filters.regex("close"))
 async def close(client, callback_query):
     await callback_query.message.delete()
